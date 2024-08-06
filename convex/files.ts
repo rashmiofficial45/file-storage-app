3bb638585aa5f6a85ce376ba6b9957cf5a1ad38d
Rashmi Ranjan Parija <rashmipersonal@Rashmis-MacBook-Air.local>
2024-08-07 00:00:37 +0530

    testing app in convex and working successfully

---
+++ convex/files.ts
@@ -0,0 +1,19 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createFile = mutation({
+    args: {
+        name: v.string()
+    },
+    async handler(ctx, args) {
+        await ctx.db.insert("files", { name: args.name });
+
+    },
+})
+
+export const getFiles = query({
+    args: {},
+    async handler(ctx) {
+        return await ctx.db.query("files").collect();
+    }
+})
