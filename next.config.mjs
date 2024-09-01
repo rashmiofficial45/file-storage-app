0aed3f1a6095203d0e19c0a9286d46ccbb6a1f6f
Rashmi Ranjan Parija <rashmipersonal@Rashmis-MacBook-Air.local>
2024-09-01 22:58:37 +0530

    deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 const nextConfig = {
     images:{
         remotePatterns:[
@@ -10,8 +8,4 @@ const nextConfig = {
         ]
     }
 };
-
-if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-  }
 export default nextConfig;
