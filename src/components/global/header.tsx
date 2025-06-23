6610469295c426b8447d40d28687e538f6991a76
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2025-06-23 11:04:19 +0530

    remove .env file from git

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,6 +1,5 @@
 import { OrganizationSwitcher, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import React from "react";
 type Props = {};
 
