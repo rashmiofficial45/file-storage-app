9e1d05ac90751052af564e70017353bd878a463f
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2025-06-25 21:20:56 +0530

    refactor: enhance header component styling and update sign-in button text

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,15 +1,17 @@
 import { OrganizationSwitcher, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
+import { Button } from "../ui/button";
+import { LogIn } from "lucide-react";
 type Props = {};
 
 const Header = async (props: Props) => {
   const user = await currentUser()
   return (
-    <div className="border-b">
+    <div className="border-b backdrop-blur-md sticky top-0 z-40 bg-slate-200/50">
       <div className="flex justify-between items-center px-8">
         <div className="font-semibold text-2xl text-slate-900 ">
-            File-store
+          File-store
         </div>
         <div className="flex justify-end py-3 items-center gap-4">
           {user ? <>
@@ -20,13 +22,13 @@ const Header = async (props: Props) => {
                 },
               }}
             />
-          <UserButton />
-          </>:<>
-          <SignedOut>
-        <SignInButton>
-          Sign In
-        </SignInButton>
-      </SignedOut>
+            <UserButton />
+          </> : <>
+            <SignedOut>
+              <SignInButton>
+                <Button variant={"default"} className=" text-base rounded-lg">Sign up</Button>
+              </SignInButton>
+            </SignedOut>
           </>}
         </div>
       </div>
